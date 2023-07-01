c70a13593aa9b3bbedb14a6bc26e05092f16b8d2
Ivan <ivangargalan@gmail.com>
2023-07-01 23:48:51 +0200

    Added a functionalty that allows you to not put the same id in the database twice.You cam now see the data in a table. CSS is not yet implemented. Bootstrap has been added.

--- index.php
+++ index.php
@@ -27,20 +27,31 @@
             $date = $_POST['date'];
             //Start the transacction in order to execute the sql queries
             $db->beginTransaction();
-            //Declare the sql query
-            $success = $db->prepare("INSERT INTO task(id,task,date) VALUES (?,?,?)");
-            //The values are here in order to avoid SQL injections.
-            $success->execute([$id,$task,$date]);
-            //Commit
-            $db->commit();
-            //If statement for seeing a success
-            if ($success) {
-                echo "Successful statement";
-            }else{
-                echo "An error has occurred.Try again later";
-                //If nothing gets added, the database does a rollback
-                $db->rollBack();
+            //Declare the query that checks if the id is in the database.
+            $newID = $id;
+            $checkID = $db->prepare("SELECT id FROM task where id = ?");
+            $checkID->execute([$newID]);
+            //Declare the sql query.Also, this if statement is here to see if the id is already
+            //in the database. If its there, shows an error message.
+            if ($checkID->rowCount() > 0) {
+                $errorID = "This ID is already in the database. You must select another";
+            } else {
+                $success = $db->prepare("INSERT INTO task(id,task,date) VALUES (?,?,?)");
+                //The values are here in order to avoid SQL injections.
+                $success->execute([$id,$task,$date]);
+                //Commit
+                $db->commit();
+                //If statement for seeing a success
+                if ($success) {
+                    echo "Successful statement";
+                }else{
+                    echo "An error has occurred.Try again later";
+                    //If nothing gets added, the database does a rollback
+                    $db->rollBack();
+                }
+                
             }
+            
         }
     }
 
@@ -55,6 +66,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/style.css">
     <title>Task manager</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 </head>
 <body>
     <!-- Header  -->
@@ -92,6 +104,38 @@
 
 
         </form>
+        <br>
+        <!-- Table for the task to appear -->
+        <table>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Task</th>
+                    <th>Date</th>
+                </tr>
+            </thead>
+            <tbody>
+                <!-- This php code fetches the databse and displays it in the table -->
+                <?php 
+                    $search = $db->prepare("select * FROM task ORDER BY `ID` ASC");
+                    $search->execute();  
+                    // While more rows are found, they appear here
+                    while ($fetch = $search->fetch()) {
+                        ?>
+                        <tr>
+                            <td><?php echo $fetch['id'] ?></td>
+                            <td><?php echo $fetch['task'] ?></td>
+                            <td><?php echo $fetch['date'] ?></td>
+                        </tr>
+
+                        <?php   
+                       } 
+                        ?>
+                
+            </tbody>
+        </table>
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
     </main>
 </body>
 </html>
